289238c2c6089badbe89ef6a4e411ee290c4583e
Hugo-eseo <70439592+Hugo-eseo@users.noreply.github.com>
2022-01-12 11:49:11 +0100

    avancement sur l'application graphique

--- appli/tft.c
+++ appli/tft.c
@@ -11,17 +11,29 @@
 // Bibliothèque de l'écran TFT
 #include "tft_ili9341/stm32f1_ili9341.h"
 
-static void TFT_draw_fat_rectangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color, uint16_t width);
 static uint8_t TFT_add_line(FontDef_t font, uint8_t spacing);
-static void TFT_add_sentence(uint16_t color, uint8_t line_number, uint8_t order, uint8_t spacing, char * string[]);
+static uint8_t TFT_add_sentence(uint8_t line_number, uint16_t color, uint8_t spacing, char * string[]);
+static void TFT_edit_sentence(uint8_t line_number, uint8_t sentence_order, uint16_t color, char * string[]);
+static uint16_t TFT_get_sentence_size(uint8_t line_number, uint8_t sentence_order);
 static void TFT_update_screen(void);
+static void TFT_create_rectangle_line(uint8_t line_number, uint8_t line_amount, uint8_t spacing, uint16_t color, uint8_t width);
 
-// Nouveau principe de fonctionnement :
+/* Principe de fonctionnement de l'affichage :
+*		- L'écran est divisé en plusieurs 'lignes'. Chaque ligne crée l'une à la suite de l'autre possède ses propores caractérisiques
+*		- Chaque ligne est ensuite subdivisée en plusieurs 'phrases' ayant également ses propres caractéristiques.
+*		Cela permet d'afficher et d'effacer facilement des informations sur l'ecran.
+*		- Enfin, les 'rectangles décoratifs' sont indépendant du système précedent et vinnent encadrer une ou plusieurs lignes.
+*/
+
+// Maximum 5 phrases/textes différents par ligne !
 #define MAX_SENTENCE_PER_LINE 5
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
 
+// 'Phrase'
 typedef struct{
 	uint16_t color; // Couleur
-	uint8_t order; // Ordre d'écriture sur la ligne, comme pour un tableau, démarre à 0 !!
+	uint8_t order; // Ordre d'écriture sur la ligne, comme pour un tableau, démarre à 1 !!
 	uint8_t spacing; // Espacement sur x avant la phrase
 	uint16_t x_cord; // Emplacement exact de la phrase
 	uint8_t size; // Taille du tableau de caractères
@@ -29,16 +41,25 @@ typedef struct{
 	char * string[]; // Phrase de maximum 30 caractères
 }sentence;
 
+// 'Ligne'
 typedef struct{
 	uint8_t spacing; // Espacement sur y avant la ligne
 	FontDef_t font; // Police d'écriture de la ligne
 	uint16_t y_cord; // Emplacement exact de la ligne (top left corner)
 	sentence sentences[MAX_SENTENCE_PER_LINE]; // Phrases à afficher sur la ligne (max 5)
+	uint8_t sentences_amount;
 }line;
 
+// L'écran peut contenir au maximum 24 lignes (calculé en fonction de sa taille : 24 lignes en police minimale
 static line screen_display[24];
 static uint8_t current_line = 0;
 
+/*
+ * @brief Ajoute une ligne dans le stockage de l'écran (attention, cette fonction n'affiche rien !
+ * @param *font : Pointer to @ref FontDef_t used font, s'applique à toute la ligne !
+ * @param spacing : Espacement AVANT l'écriture du texte (interligne supérieure)
+ * @ret Le numéro de la ligne qui devra être utilisé pour 'add_sentence'
+ */
 static uint8_t TFT_add_line(FontDef_t font, uint8_t spacing){
 	line * line_ref = &screen_display[current_line];
 	uint16_t y = current_line==0?0:screen_display[current_line-1].y_cord+screen_display[current_line-1].font.FontHeight;
@@ -46,12 +67,28 @@ static uint8_t TFT_add_line(FontDef_t font, uint8_t spacing){
 	line_ref->font = font;
 	line_ref->spacing = spacing;
 	line_ref->y_cord = y += spacing;
+	line_ref->sentences_amount = 0;
 	return current_line;
 }
 
-static void TFT_add_sentence(uint16_t color, uint8_t line_number, uint8_t order, uint8_t spacing, char * string[]){
-	sentence * sentence_ref = &screen_display[line_number].sentences[order];
-	uint16_t x = order==0?0:screen_display[line_number].sentences[order-1].x_cord + (screen_display[line_number].sentences[order-1].size*screen_display[line_number].font.FontWidth);
+/*
+ * @biref Ajoute du texte sur le ligne donnée (attention, cette fonction ne met pas à jours l'écran !)
+ * @param line_number : numéro de la ligne consernée
+ * @param color : couleur du texte souhaitée
+ * @param spacing : espacement en x avec la phrase précedente ou le bord de l'écran
+ * @param * string[] : tableau de caractères (String) contenant la phrase à écrire
+ * @ret L'ordre de la phrase dans la ligne (de 1 à MAX_SENTENCE_PER_LINE inclus)
+ */
+static uint8_t TFT_add_sentence(uint8_t line_number, uint16_t color, uint8_t spacing, char * string[]){
+	line * line_ref = &screen_display[line_number];
+	line_ref->sentences_amount += 1;
+	uint8_t order = line_ref->sentences_amount;
+
+	// Si l'on dépasse le nombre de phrases par ligne, on ne fait rien
+	if(order > MAX_SENTENCE_PER_LINE) return;
+
+	sentence * sentence_ref = &line_ref->sentences[order];
+	uint16_t x = order==1?0:line_ref->sentences[order-1].x_cord + TFT_get_sentence_size(line_number, line_ref->sentences[order-1].order);
 	sentence_ref->color = color;
 	sentence_ref->order = order;
 	sentence_ref->spacing = spacing;
@@ -61,7 +98,55 @@ static void TFT_add_sentence(uint16_t color, uint8_t line_number, uint8_t order,
 	for(int i=0; i<sentence_ref->size; i++){
 		sentence_ref->string[i] = string[i];
 	}
+	return order;
 }
+
+/*
+ * @brief Modifie une phrase déjà créer et ajuste la disposition des phrases suivantes
+ * @param line_number : numéro de la ligne consernée
+ * @param sentence_ordrer : ordre de la phrase concernée
+ * @param color : couleur du texte souhaitée
+ * @param * string[] : tableau de caractères (String) contenant la phrase à écrire
+ */
+
+static void TFT_edit_sentence(uint8_t line_number, uint8_t sentence_order, uint16_t color, char * string[]){
+	line * line_ref = &screen_display[line_number];
+	sentence * sentence_ref = &line_ref->sentences[sentence_order];
+	sentence_ref->color = color;
+	sentence_ref->updated = TRUE;
+	for(int i=0; i<sentence_ref->size; i++){
+		sentence_ref->string[i] = string[i];
+	}
+	sentence_ref->size = sizeof(string);
+	for(int i=1; i<=line_ref->sentences_amount-sentence_order; i++){
+		sentence_ref = &line_ref->sentences[sentence_order+i];
+		sentence * previous_sentence_ref = &line_ref->sentences[sentence_order+i-1];
+		sentence_ref->x_cord = previous_sentence_ref->x_cord + TFT_get_sentence_size(line_number, previous_sentence_ref->order) + sentence_ref->spacing;
+		sentence_ref->updated = TRUE;
+	}
+}
+
+/*
+ * @brief Renvoie en pixel la taille d'une phrase
+ * @param line_number : numéro de la ligne consernée
+ * @param sentence_ordrer : ordre de la phrase concernée
+ * @ret La taille de la phrase en pixel
+ */
+static uint16_t TFT_get_sentence_size(uint8_t line_number, uint8_t sentence_order){
+	line * line_ref = &screen_display[line_number];
+	sentence * sentence_ref = &line_ref->sentences[sentence_order];
+	return(sentence_ref->size*line_ref->font.FontWidth);
+}
+
+// A terminer !
+/*
+static void TFT_delete_sentence(void){
+	// A compléter
+}
+
+static void TFT_delete_line(void){
+	// A compléter
+}*/
 
 static void TFT_update_screen(void){
 	for(int i=0; i<current_line+1; i++){
@@ -74,6 +159,22 @@ static void TFT_update_screen(void){
 			}
 		}
 	}
+}
+
+static void TFT_create_rectangle_line(uint8_t line_number, uint8_t line_amount, uint8_t spacing, uint16_t color, uint8_t width){
+	line * line_ref = &screen_display[line_number];
+	uint8_t x0 = spacing;
+	uint8_t y0 = line_ref->y_cord - spacing;
+	uint8_t x1 = SCREEN_WIDTH - spacing;
+	uint8_t y1 = line_ref->y_cord + line_ref->font.FontHeight + spacing;
+	for(int i=1; i<=line_amount; i++){
+		line * line_ref = &screen_display[line_number+i];
+		y1 += line_ref->spacing + line_ref->font.FontHeight;
+	}
+	ILI9341_DrawFilledRectangle(x0, y0, x1, y0+width, color);
+	ILI9341_DrawFilledRectangle(x0, y0, x0+width, y1, color);
+	ILI9341_DrawFilledRectangle(x1-width, y0, x1, y1, color);
+	ILI9341_DrawFilledRectangle(x0, y1-width, x1, y1, color);
 }
 
 void TFT_init(void){
@@ -126,7 +227,7 @@ void TFT_init(void){
 		ILI9341_Puts(FONT_WIDTH + 4, line, &FIRST_RECTANGLE[i][0], FONT_USED, ILI9341_COLOR_BLACK, ILI9341_COLOR_WHITE);
 		line += FONT_HEIGHT + LINES_SPACE;
 	}
-	TFT_draw_fat_rectangle(FONT_WIDTH, saved_line, 320 - FONT_WIDTH, line + LINES_SPACE, ILI9341_COLOR_RED, 2);
+	// TFT_draw_fat_rectangle(FONT_WIDTH, saved_line, 320 - FONT_WIDTH, line + LINES_SPACE, ILI9341_COLOR_RED, 2);
 
 	// Second cadre d'information sur la consommation en cours
 	saved_line = line + 2*LINES_SPACE;
@@ -135,7 +236,7 @@ void TFT_init(void){
 		ILI9341_Puts(FONT_WIDTH + 4, line, &SECOND_RECTANGLE[i][0], FONT_USED, ILI9341_COLOR_BLACK, ILI9341_COLOR_WHITE);
 		line += FONT_HEIGHT + LINES_SPACE;
 	}
-	TFT_draw_fat_rectangle(FONT_WIDTH, saved_line, 320 - FONT_WIDTH, line + LINES_SPACE, ILI9341_COLOR_BLUE, 2);
+	// TFT_draw_fat_rectangle(FONT_WIDTH, saved_line, 320 - FONT_WIDTH, line + LINES_SPACE, ILI9341_COLOR_BLUE, 2);
 
 	// Troisième cadre contenant l'historique
 	saved_line = line + 2*LINES_SPACE;
@@ -144,15 +245,7 @@ void TFT_init(void){
 		ILI9341_Puts(FONT_WIDTH + 4, line, &THIRD_RECTANGLE[i][0], FONT_USED, ILI9341_COLOR_BLACK, ILI9341_COLOR_WHITE);
 		line += FONT_HEIGHT + LINES_SPACE;
 	}
-	TFT_draw_fat_rectangle(FONT_WIDTH, saved_line, 320 - FONT_WIDTH, line + LINES_SPACE, ILI9341_COLOR_MAGENTA, 2);
-}
-
-
-static void TFT_draw_fat_rectangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color, uint16_t width){
-	ILI9341_DrawFilledRectangle(x0, y0, x1, y0+width, color);
-	ILI9341_DrawFilledRectangle(x0, y0, x0+width, y1, color);
-	ILI9341_DrawFilledRectangle(x1-width, y0, x1, y1, color);
-	ILI9341_DrawFilledRectangle(x0, y1-width, x1, y1, color);
+	// TFT_draw_fat_rectangle(FONT_WIDTH, saved_line, 320 - FONT_WIDTH, line + LINES_SPACE, ILI9341_COLOR_MAGENTA, 2);
 }
 
 void TFT_update_info(void){
